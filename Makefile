a101de979c78005b48b2bc728bdb21df8c10d333
Philipp Hagemeister <phihag@phihag.de>
2020-04-18 21:09:11 +0200

    demonstration for https://stackoverflow.com/q/61294426/35070

---
+++ Makefile
@@ -0,0 +1,7 @@
+doc: demo.js package.json
+	@npm run doc
+
+clean:
+	rm -rf doc
+
+.PHONY: clean
---
+++ demo.js
@@ -0,0 +1,8 @@
+/**
+ * FOO
+ * @param {Number} foo An interesting value
+ * @param {Number} __namedParameters.bar Another value
+ */
+function demo({ foo=42, bar=43 } = {}) { }
+
+module.exports = {demo};
